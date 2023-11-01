2186559772cfbc4cf1dc0074a86de082eb18bfaa
sleeptightwakebright@hotmail.com <sleeptightwakebright@hotmail.com>
2023-11-01 21:12:05 +1100

    added brute force getTarget function and layout randomiser

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const field = document.getElementById('theField');
+
+
+const populateField = (num) => {
+    for (let i = 0; i < num; i++) {
+        let whichItem = i % 3;
+        let itemX = Math.random() * window.innerWidth;
+        let itemY = Math.random() * window.innerHeight;
+        if (whichItem === 0) {
+            field.innerHTML += `<div class="item rock" id="${i}" style="left:${itemX}px; top:${itemY}px;"></div>`;
+        }
+        else if (whichItem === 1) {
+            field.innerHTML += `<div class="item paper" id="${i}" style="left:${itemX}px; top:${itemY}px;"></div>`;
+        }
+        else {
+            field.innerHTML += `<div class="item scissors" id="${i}" style="left:${itemX}px; top:${itemY}px;"></div>`;
+        }
+    }
+}
+populateField(10);
+const exampleItem = document.getElementById('1');
+
+
 
 const getDirection = (originItem, destinationItem) => {
     let destinationObject = getComputedStyle(destinationItem);
@@ -9,36 +32,44 @@ const getDirection = (originItem, destinationItem) => {
     let originItemX = parseFloat(originObject.left);
 
     let angle = Math.atan2(destinationY - originItemY, destinationX - originItemX) * 180 / Math.PI;
-    return angle
+    return angle;
 }
-
-const scissors = document.getElementById('1');
-const paper = document.getElementById('2');
-const rock = document.getElementById('3');
-
-getDirection(rock, scissors);
-getDirection(scissors, paper);
 
 const getTarget = (item) => {
-    let theClassList = item.classList;
-    if (theClassList.contains('rock')) {
-        return scissors;
-    }
-    else if (theClassList.contains('paper')) {
-        return rock;
-    }
-    else if (theClassList.contains('scissors')) {
-        return paper;
-    }
-    else {
-        return null;
-    }
+    let theClass = item.classList[1];
+    let targetClass = "0";
+    if (theClass == "rock") {targetClass = "scissors";}
+    if (theClass == "paper") {targetClass = "rock";}
+    if (theClass == "scissors") {targetClass = "paper";}
 
+    let itemObject = getComputedStyle(item);
+    let itemX = parseFloat(itemObject.left);
+    let itemY = parseFloat(itemObject.top);
+    
+    let targetList = document.getElementsByClassName(targetClass);
+    let closestTarget = null;
+    let closestDistance = null;
+
+    //loop through targetList to find closest target
+    for (let i = 0; i < targetList.length; i++) {
+        let targetObject = getComputedStyle(targetList[i]);
+        let targetX = parseFloat(targetObject.left);
+        let targetY = parseFloat(targetObject.top);
+        let distance = Math.sqrt(Math.pow(targetX - itemX, 2) + Math.pow(targetY - itemY, 2));
+        console.log({"distance": distance, "target": targetList[i]})
+        if (distance < closestDistance || closestDistance === null) {
+            closestDistance = distance;
+            closestTarget = targetList[i];
+        }
+    }
+    console.log("the closest distance is " + closestDistance);
+    return closestTarget;
+    
 }
-
+getTarget(exampleItem);
 
 const moveItem = (item) => {
-    let distance = 10;
+    let distance = 1;
     let target = getTarget(item);
     let angle = getDirection(item, target);
 
@@ -46,20 +77,16 @@ const moveItem = (item) => {
     let y = distance * Math.sin(angle * Math.PI / 180);
     let newPosX = parseFloat(getComputedStyle(item).left) + x;
     let newPosY = parseFloat(getComputedStyle(item).top) + y;
-    console.log(newPosX, newPosY);
     item.style.left = newPosX + 'px';
     item.style.top = newPosY + 'px';
 }
 
-setInterval(
-    () => moveItem(rock),
-    500
-);
-setInterval(
-    () => moveItem(scissors),
-    500
-);
-setInterval(
-    () => moveItem(paper),
-    500
-);
+
+const runTimestep = () => {
+    let items = field.children;
+    items[0].style.backgroundColor = 'red';
+    setInterval(() => {
+        moveItem(items[0]);
+    }, 100);
+}
+runTimestep();
